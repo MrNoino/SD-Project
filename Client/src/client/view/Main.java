9d9d57e29a6408550e0f8750cdc45ed522cbf288
MrNoino <nuno.miguelsl25@gmail.com>
2024-04-26 11:30:20 +0100

    Added gitignore and create the client and server aplications

---
+++ Client/src/client/view/Main.java
@@ -0,0 +1,7 @@
+package client.view;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Server/src/java/application/ServerApplication.java
@@ -0,0 +1,21 @@
+package application;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("api")
+public class ServerApplication extends Application {
+
+    private Set<Object> singletons = new HashSet<Object>();
+
+    public ServerApplication() {
+        singletons.add(new Chess());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
